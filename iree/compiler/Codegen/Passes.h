437d233b16ab43dd648568455bee5517ff599765
stanley <stanley@nod-labs.com>
2022-02-04 04:34:35 +0000

    Warp Reduce

--- iree/compiler/Codegen/Passes.h
+++ iree/compiler/Codegen/Passes.h
@@ -256,6 +256,9 @@ void addGPUMatmulSimtPassPipeline(OpPassManager &pm);
 /// Lowering using tensorcore operations.
 void addGPUMatmulTensorCorePassPipeline(OpPassManager &pm);
 
+// Lowering to enable Warp reduction.
+void addGPUWarpLevelReductionPassPipeline(OpPassManager &pm);
+
 /// Simple lowering only distributute linalg ops on blocks and threads. This
 /// will result in scalar operations. Expects pass manager to be a module-level
 /// pass manager.
@@ -304,6 +307,11 @@ std::unique_ptr<OperationPass<FuncOp>> createLLVMGPUMultiBuffering(
 
 /// Converts vector ops to gpu dialect.
 std::unique_ptr<OperationPass<FuncOp>> createLLVMGPUVectorToGPU();
+
+std::unique_ptr<OperationPass<FuncOp>> createConvertVectorReductionToGPUPass();
+
+std::unique_ptr<OperationPass<FuncOp>>
+createLLVMGPUReductionVectorizationPass();
 
 //------------------------------------------------------------------------------
 // SPIR-V Passes
