188690c7eef9c379e23b5b46372313ed929af61c
Stanley Winata <stanley@nod-labs.com>
2022-07-01 15:18:44 -0700

    [Intel LevelZero] Add LevelZero to HAL and Codegen
    Use flags -DIREE_BUILD_EXPERIMENTAL_LEVEL_ZERO=ON -DLEVEL_ZERO_HEADERS_API_ROOT=/home/stanley/nod/level-zero
    -LevelZero HAL Driver
    -Addi OpenCL HAL Target compiler
    -Add SPIRV Codegen for Kernel capability

--- compiler/src/iree/compiler/Codegen/SPIRV/Passes.cpp
+++ compiler/src/iree/compiler/Codegen/SPIRV/Passes.cpp
@@ -177,7 +177,8 @@ static void addMemRefLoweringPasses(OpPassManager &pm) {
 }
 
 /// Adds passes to perform the final SPIR-V conversion.
-static void addSPIRVLoweringPasses(OpPassManager &pm) {
+static void addSPIRVLoweringPasses(OpPassManager &pm,
+                                   bool useKernelCapability = false) {
   pm.addPass(createCanonicalizerPass());
   pm.addPass(createCSEPass());
 
@@ -185,7 +186,11 @@ static void addSPIRVLoweringPasses(OpPassManager &pm) {
   pm.addPass(createCanonicalizerPass());
   pm.addPass(createCSEPass());
 
-  pm.addPass(createMapMemRefStorageClassPass());
+  if (useKernelCapability)
+    pm.addPass(createMapMemRefStorageClassPass(
+        spirv::mapMemorySpaceToOpenCLStorageClass));
+  else
+    pm.addPass(createMapMemRefStorageClassPass());
   pm.addPass(createConvertToSPIRVPass());
 
   OpPassManager &spirvPM = pm.nest<spirv::ModuleOp>();
@@ -317,13 +322,14 @@ void addSPIRVTileAndDistributePassPipeline(OpPassManager &pm) {
 // Entry Point
 //===----------------------------------------------------------------------===//
 
-void buildSPIRVCodegenPassPipeline(OpPassManager &pm) {
+void buildSPIRVCodegenPassPipeline(OpPassManager &pm,
+                                   bool useKernelCapability) {
   pm.nest<ModuleOp>().nest<func::FuncOp>().addPass(createTypePropagationPass());
   pm.nest<ModuleOp>().addPass(createBufferizeCopyOnlyDispatchesPass());
   pm.addPass(createSPIRVLowerExecutableTargetPass());
 
   addMemRefLoweringPasses(pm.nest<ModuleOp>());
-  addSPIRVLoweringPasses(pm.nest<ModuleOp>());
+  addSPIRVLoweringPasses(pm.nest<ModuleOp>(), useKernelCapability);
 
   LLVM_DEBUG({
     llvm::dbgs() << "Using SPIR-V pass pipeline:\n";
