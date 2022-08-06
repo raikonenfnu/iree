28b14a87f591da5d2e3527c65ede71f9eb1a6f72
Anush Elangovan <anush@nod-labs.com>
2022-08-06 18:41:37 +0000

    Temp fix to disable tracy

--- build_tools/python_deploy/build_linux_packages.sh
+++ build_tools/python_deploy/build_linux_packages.sh
@@ -122,7 +122,6 @@ function build_iree_runtime() {
 function build_iree_runtime_instrumented() {
   IREE_HAL_DRIVER_CUDA=ON \
   IREE_EXTERNAL_HAL_DRIVERS=rocm \
-  IREE_BUILD_TRACY=ON IREE_ENABLE_RUNTIME_TRACING=ON \
   IREE_RUNTIME_CUSTOM_PACKAGE_SUFFIX="-instrumented" \
   python -m pip wheel -v -w /wheelhouse /main_checkout/iree/runtime/
 }
