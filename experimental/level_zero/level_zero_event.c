96b697b6e97f057d2367137e4adb71d11887d8f7
Stanley Winata <stanley@nod-labs.com>
2022-09-01 18:24:26 -0700

    [hal][level-zero] Adding events synchronization

--- experimental/level_zero/level_zero_event.c
+++ experimental/level_zero/level_zero_event.c
@@ -15,6 +15,7 @@
 typedef struct iree_hal_level_zero_event_t {
   iree_hal_resource_t resource;
   iree_hal_level_zero_context_wrapper_t* context_wrapper;
+  ze_event_handle_t handle;
 } iree_hal_level_zero_event_t;
 
 static const iree_hal_event_vtable_t iree_hal_level_zero_event_vtable;
@@ -27,6 +28,7 @@ static iree_hal_level_zero_event_t* iree_hal_level_zero_event_cast(
 
 iree_status_t iree_hal_level_zero_event_create(
     iree_hal_level_zero_context_wrapper_t* context_wrapper,
+    ze_event_pool_handle_t event_pool,
     iree_hal_event_t** out_event) {
   IREE_ASSERT_ARGUMENT(context_wrapper);
   IREE_ASSERT_ARGUMENT(out_event);
@@ -39,6 +41,15 @@ iree_status_t iree_hal_level_zero_event_create(
   if (iree_status_is_ok(status)) {
     iree_hal_resource_initialize(&iree_hal_level_zero_event_vtable,
                                  &event->resource);
+    ze_event_desc_t event_desc = {};
+    event_desc.stype = ZE_STRUCTURE_TYPE_EVENT_DESC;
+    event_desc.signal = ZE_EVENT_SCOPE_FLAG_HOST;
+    event_desc.wait = ZE_EVENT_SCOPE_FLAG_HOST;
+    ze_event_handle_t handle;
+    LEVEL_ZERO_RETURN_IF_ERROR(
+        context_wrapper->syms,
+        zeEventCreate(event_pool, &event_desc, &handle), "zeEventCreate");
+    event->handle = handle;
     event->context_wrapper = context_wrapper;
     *out_event = (iree_hal_event_t*)event;
   }
@@ -52,10 +63,16 @@ static void iree_hal_level_zero_event_destroy(iree_hal_event_t* base_event) {
       iree_hal_level_zero_event_cast(base_event);
   iree_allocator_t host_allocator = event->context_wrapper->host_allocator;
   IREE_TRACE_ZONE_BEGIN(z0);
-
+  LEVEL_ZERO_IGNORE_ERROR(event->context_wrapper->syms,
+                        zeEventDestroy(event->handle));
   iree_allocator_free(host_allocator, event);
 
   IREE_TRACE_ZONE_END(z0);
+}
+
+ze_event_handle_t iree_hal_level_zero_event_handle(
+    const iree_hal_event_t* base_event) {
+  return ((const iree_hal_level_zero_event_t*)base_event)->handle;
 }
 
 static const iree_hal_event_vtable_t iree_hal_level_zero_event_vtable = {
