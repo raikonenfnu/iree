5ee41c4d9f1cdcf407ebafb12586f53edc3fddb8
Stanley Winata <stanley@nod-labs.com>
2023-08-27 11:49:39 -0700

    [ROCM] Enable WarpReduction on ROCM + Matvec on GPU.

--- compiler/src/iree/compiler/Codegen/LLVMGPU/ConvertToROCDL.cpp
+++ compiler/src/iree/compiler/Codegen/LLVMGPU/ConvertToROCDL.cpp
@@ -117,7 +117,6 @@ struct ConvertToROCDLPass : public ConvertToROCDLBase<ConvertToROCDLPass> {
       if (failed(applyPartialConversion(m, target, std::move(llvmPatterns))))
         signalPassFailure();
     }
-    ConvertToDynamicSharedMemory(m);
   }
 };
 
