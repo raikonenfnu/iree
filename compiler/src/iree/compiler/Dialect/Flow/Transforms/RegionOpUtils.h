cba94a5a0561dfdaea368b5f705b2400de051d60
Stanley Winata <stanley@nod-labs.com>
2023-11-02 16:10:15 -0700

    Refactoring to use dequant check in fusionOfTensors to enable piepline
    In pipeline test in flow dialect, the input to dequant is not
    globalLoad, but ratherc coming in from a Fn argument. Now we just check
    for dequant rather than global load.

--- compiler/src/iree/compiler/Dialect/Flow/Transforms/RegionOpUtils.h
+++ compiler/src/iree/compiler/Dialect/Flow/Transforms/RegionOpUtils.h
@@ -96,6 +96,12 @@ FailureOr<Flow::DispatchRegionOp> wrapOpInDispatchRegion(RewriterBase &rewriter,
 /// into a dispatch region.
 bool isClonableIntoDispatchOp(Operation *op);
 
+/// Returns true if the operation is an generic op that represents dequant.
+bool isGroupedDequantizationOp(Operation *op);
+
+/// Returns true if the operation is a matmul or matmul-like op.
+bool isMatmulLikeOp(Operation *op);
+
 /// Clone into the region producers of those value used in the region but
 /// defined above, to prepare the dispatch region isolated from above.
 LogicalResult cloneProducersToRegion(RewriterBase &rewriter,
