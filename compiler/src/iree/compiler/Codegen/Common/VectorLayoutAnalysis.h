0a7d048a3b6566ee433706a985e82a509bc2e46c
Kunwar Grover <groverkss@gmail.com>
2023-12-08 06:36:57 +0530

    Distribuet to shared memory if layout cannot be resolved

--- compiler/src/iree/compiler/Codegen/Common/VectorLayoutAnalysis.h
+++ compiler/src/iree/compiler/Codegen/Common/VectorLayoutAnalysis.h
@@ -135,10 +135,9 @@ public:
     return cast<T>(layout);
   }
 
-  // Duplicate the layout information of oldValue to newValue. This operation
-  // assumes that oldValue already has a layout known the analysis. newValue
-  // must not have a layout known to the analysis.
   void cloneLayoutInformationToNewValue(Value oldValue, Value newValue);
+  void cloneLayoutInformationToNewValue(VectorLayoutInterface layout,
+                                        Value val);
 
   void print(raw_ostream &os);
   void dump() {
