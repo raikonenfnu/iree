aec57e7e5d927ce28b10e8555dbe3ae116dd7a75
Kunwar Grover <groverkss@gmail.com>
2024-05-06 01:25:05 +0000

    Add option to pad-to-intrinsics to pad to a multiple of intrinsic

--- scripts/compile_matmul.sh
+++ scripts/compile_matmul.sh
@@ -9,7 +9,7 @@ shift
 
 ./tools/iree-compile "$INPUT" \
   --iree-hal-target-backends=rocm --iree-rocm-target-chip=gfx1100 \
-  --iree-preprocessing-pass-pipeline="builtin.module(util.func(iree-preprocessing-pad-to-intrinsics))" \
+  --iree-preprocessing-pass-pipeline="builtin.module(util.func(iree-preprocessing-pad-to-intrinsics{intrinsic-multiple=4}))" \
   --iree-codegen-llvmgpu-use-vector-distribution \
   --iree-llvmgpu-enable-prefetch \
   --iree-stream-resource-max-allocation-size=4294967296 \
