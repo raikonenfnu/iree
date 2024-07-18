306357381f07ad1dd3b327092c470ff7cc7d7a4a
Stanley Winata <stanley.winata@amd.com>
2024-07-18 12:36:09 -0500

    Working attention CPP pipeline

--- compiler/src/iree/compiler/Codegen/Common/GPU/GPUNestedLayoutDistributionPatterns.cpp
+++ compiler/src/iree/compiler/Codegen/Common/GPU/GPUNestedLayoutDistributionPatterns.cpp
@@ -467,17 +467,21 @@ struct DistributeMultiReduction final
     SmallVector<int64_t> flatShape(1, numElements);
     VectorType flatVecType = VectorType::get(flatShape, elemTy);
     VectorValue flat =
-        rewriter.create<vector::ShapeCastOp>(loc, flatVecType, locallyReduced);
+        rewriter.create<vector::ShapeCastOp>(loc, flatVecType, disSrc);
 
     FailureOr<VectorValue> threadReduced = doThreadReduction(
         rewriter, srcLayout, flat, multiReduceOp.getKind(), reducedDims);
     if (failed(threadReduced)) {
       return failure();
     }
-
     VectorValue unflattened = rewriter.create<vector::ShapeCastOp>(
         loc, shaped, threadReduced.value());
-    replaceOpWithDistributedValues(rewriter, multiReduceOp, unflattened);
+    Value nullMask;
+    Value finalReduction =
+        vector::makeArithReduction(rewriter, loc, multiReduceOp.getKind(),
+                                   unflattened, disAcc, nullptr, nullMask);
+    auto finalReduced = dyn_cast<VectorValue>(finalReduction);
+    replaceOpWithDistributedValues(rewriter, multiReduceOp, finalReduced);
 
     return failure();
   }
